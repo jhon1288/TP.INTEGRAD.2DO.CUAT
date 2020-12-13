728027963f5120359805c8305d47ee832883ca60
jhon1288 <75097281+jhon1288@users.noreply.github.com>
2020-12-13 10:50:14 -0300

    Update TP2inicio.cpp

---
+++ TP2INTEGRAD.cpp/TP2inicio.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<stdlib.h>
+#include<ctype.h>
+
+  FILE*fp;
+  
+  FILE*fm;
+  FILE*ft;
+  
+void crearUsuario();
+void loginUsuario();
+void menuPrincipal();
+void registrarveterinario();
+void registrarmascota();
+void registrarTurno();
