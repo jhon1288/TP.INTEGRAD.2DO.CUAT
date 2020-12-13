b5b74746af253765c4b88ebdcc303da68ef5477c
jhon1288 <75097281+jhon1288@users.noreply.github.com>
2020-12-13 10:51:52 -0300

    Update TP2inicio.cpp
    creacion de usuario

--- TP2INTEGRAD.cpp/TP2inicio.cpp
+++ TP2INTEGRAD.cpp/TP2inicio.cpp
@@ -13,3 +13,70 @@ void menuPrincipal();
 void registrarveterinario();
 void registrarmascota();
 void registrarTurno();
+void listaVetPorfecha();
+void listaTurno();
+void llamarmascota();
+
+typedef char cadena[50];
+
+struct Fecha{
+	
+	int dia, mes,anio;
+}; 
+
+struct FechadeNac{
+	
+	int dia,mes,anio;
+}; 
+
+struct Usuarios{
+	
+	cadena usuario, contrasenea;
+	char Apellidoynombre[60];
+	
+};
+
+struct Veterinario{
+	
+	char ApellidoyNombre[60];
+	char domicilio[60];
+	int DNIdueneo;
+	char localidad[60];
+	FechadeNacimiento fecha;
+	float peso;
+	int Telefono;
+	
+};
+
+struct Turnos{
+	
+	int MatriculadeVeterinario;
+	Fecha fecha;
+	int DNIdueneo;
+	char detalledeAtencion[380];
+};
+main(){
+	
+	int n;
+	while(true)
+	{
+		system("cls");
+		
+		printf("\n\n\n\n\n\n\n\n\t\t1-registrarse.\n\t\t\t\t2-login.\n\t\t\t\t3-salir.\n\t\t\t\telija una opcion :");
+		scanf("%d",&n);
+		_flushall();
+		
+		switch(n)
+		{
+			case 1 : crearUsuario();
+			    break;
+			    
+				
+		}
+	}
+}
+	
+	
+};
+
+
